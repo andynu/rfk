de6ef5264abed28143335403dbaee1a77f8608c9
Andy <andy@wi.mit.edu>
2015-11-11 14:50:52 -0500

    move library initialization to the package

--- src/rfk/library/library.go
+++ src/rfk/library/library.go
@@ -8,14 +8,20 @@ import (
 
 var songsList []string
 
+func init() {
+	LoadSongs("./data/mongongo/songs.txt")
+}
+
 func LoadSongs(songsTxt string) *[]string {
 	f, err := os.Open(songsTxt)
 	check(err)
 	defer f.Close()
+
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		songsList = append(songsList, scanner.Text())
 	}
+
 	return &songsList
 }
 
--- src/rfk/rfk.go
+++ src/rfk/rfk.go
@@ -2,21 +2,14 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"rfk/dj"
-	"rfk/library"
 	"rfk/player"
 )
 
-var songsTxt = flag.String("c", "/home/andy/songs.txt", "Songs txt file, one path per line")
-
 func main() {
-	flag.Parse()
-	library.LoadSongs(*songsTxt)
 	log.Println("RFK v3 startup")
 	for {
-		song := dj.NextSong()
-		player.Play(song)
+		player.Play(dj.NextSong())
 	}
 }
