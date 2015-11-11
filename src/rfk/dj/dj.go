9f903d57173f8d6e4988bacd6824242e608ab4fb
Andy <andy@wi.mit.edu>
2015-11-11 14:51:07 -0500

    groundwork for requests

--- src/rfk/dj/dj.go
+++ src/rfk/dj/dj.go
@@ -2,22 +2,44 @@
 package dj
 
 import (
+	"container/list"
+	"errors"
 	"math/rand"
 	"rfk/library"
 	"time"
 )
 
 var songs []string
+var requests = list.New()
 
 func NextSong() string {
 	if songs == nil {
 		songs = *library.Songs()
 	}
-	return randomSong()
+
+	next_song, err := requestedSong()
+	if err != nil {
+		next_song, _ = randomSong()
+	}
+
+	return next_song
 }
 
-func randomSong() string {
+func randomSong() (string, error) {
 	rand.Seed(time.Now().UnixNano())
 	idx := rand.Intn(len(songs))
-	return songs[idx]
+	return songs[idx], nil
+}
+
+func requestedSong() (string, error) {
+	if requests.Len() >= 0 {
+		//return pop(requests).Value.(string), nil
+	}
+	return "", errors.New("NoRequests")
+}
+
+func pop(list *list.List) *list.Element {
+	e := list.Back()
+	list.Remove(e)
+	return e
 }
