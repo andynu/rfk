b6b2309c16584108290411b21c2eafbc706ffefa
Andy <andy@wi.mit.edu>
2015-11-12 15:09:57 -0500

    show mp3 meta data on the console when playing

--- src/rfk/player/player.go
+++ src/rfk/player/player.go
@@ -3,16 +3,40 @@ package player
 
 import (
 	"log"
+	"os"
 	"os/exec"
+
+	"github.com/dhowden/tag"
 )
 
 const playerBin string = "mpg123"
 
 func Play(path string) {
-  log.Printf("player: playing %q", path)
+	log.Printf("player: playing %q", path)
+	logMetadata(path)
+
 	out, err := exec.Command(playerBin, path).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf(string(out))
+}
+
+func logMetadata(path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Printf("Could not load metadata: %q", err)
+		return
+	}
+	defer f.Close()
+
+	m, err := tag.ReadFrom(f)
+	if err != nil {
+		log.Printf("Could not load metadata: %q", err)
+		return
+	}
+
+	log.Printf("\tTitle: %q", m.Title())
+	log.Printf("\tAlbum: %q", m.Album())
+	log.Printf("\tArtist: %q", m.Artist())
 }
