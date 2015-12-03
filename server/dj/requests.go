cb8df7128dbf9e17b289af0d7b655f7695daa76a
Andy <andy@wi.mit.edu>
2015-12-03 18:01:40 -0500

    cleanup requests with new type requestList

--- server/dj/requests.go
+++ server/dj/requests.go
@@ -4,39 +4,61 @@ import (
 	"container/list"
 	"errors"
 
-	"github.com/andynu/rfk/server/library"
 	"sync"
+
+	"github.com/andynu/rfk/server/library"
 )
 
-var requests_mu sync.Mutex
-var requests = list.New()
+type requestList struct {
+	list.List
+	sync.Mutex
+}
 
-func requestedSong() (library.Song, error) {
-	requests_mu.Lock()
-	defer func() { requests_mu.Unlock() }()
-	if requests.Len() >= 0 {
-		song := pop(requests).Value.(library.Song)
-		return song, nil
+var requests requestList = requestList{}
+
+
+func (list *requestList) popSong() (library.Song, error) {
+	if list.Len() > 0 {
+		song, ok := requests.pop().Value.(library.Song)
+		if ok {
+			return song, nil
+		}
 	}
 	return library.Song{}, errors.New("NoRequests")
 }
 
-func pop(list *list.List) *list.Element {
+func (list *requestList) pop() *list.Element {
 	e := list.Back()
 	list.Remove(e)
 	return e
 }
 
-func Request(songs []*library.Song) {
-	requests_mu.Lock()
+func (list *requestList) addAll(songs []*library.Song) {
 	for _, song := range songs {
-		requests.PushBack(*song)
+		list.PushBack(*song)
 	}
-	requests_mu.Unlock()
+}
+
+
+func PopRequest() (library.Song, error) {
+	requests.Lock()
+	defer requests.Unlock()
+
+	return requests.popSong()
+}
+
+func Request(songs []*library.Song) {
+	requests.Lock()
+	defer requests.Unlock()
+
+	requests.addAll(songs)
 }
 
 func ClearRequests() {
-	requests_mu.Lock()
-	requests = list.New()
-	requests_mu.Unlock()
+	requests = requestList{}
+}
+
+// see dj.NextSong()
+func requestedSong() (library.Song, error) {
+	return PopRequest()
 }
