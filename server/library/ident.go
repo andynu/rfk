a1e7a8fd0f145d1584200db4b5fe343da47144b8
Andy <andy@wi.mit.edu>
2016-02-02 17:41:23 -0500

    Record unhashible songs as SongErr, do not attempt to load those songs.
    Some songs can't be hashed. Write those paths to song_errors.txt. Load
    them up in a map on intialization and skip past those paths when loading
    songs.
    
    Write song_hashes.txt (hash, path, metadata) using a csv.NewWriter to
    handle string quoting appropriately.
    
    Ignore ctags file.

--- server/library/ident.go
+++ server/library/ident.go
@@ -33,6 +33,7 @@ package library
 //
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
@@ -43,15 +44,26 @@ import (
 	"github.com/dhowden/tag"
 )
 
+type SongErr struct {
+	Song *Song
+	Err  error
+}
+
 func IdentifySongs(songs []*Song, outFile string) {
 
 	log.Printf("Identifying songs...")
+
+	if len(songs) == 0 {
+		log.Printf("No songs to identify.")
+		return
+	}
 
 	// How many identification routines can run concurrently
 	const concurrency = 10
 	var sem = make(chan bool, concurrency)
 
 	var songsOutCh = make(chan *Song)
+	var songsErrCh = make(chan SongErr)
 	var wg sync.WaitGroup
 
 	i := 0
@@ -67,6 +79,10 @@ func IdentifySongs(songs []*Song, outFile string) {
 					defer func() { wg.Done() }()
 					hash, err := audioFileChecksum(song.Path)
 					if err != nil {
+						var songErr SongErr
+						songErr.Song = song
+						songErr.Err = err
+						songsErrCh <- songErr
 						log.Printf("ident: err: %v for %s", err, song.Path)
 						return
 					}
@@ -82,6 +98,23 @@ func IdentifySongs(songs []*Song, outFile string) {
 	go func() {
 		wg.Wait()
 		close(songsOutCh)
+		close(songsErrCh)
+	}()
+
+	// Consumes from songsErrCh and writes song_errors.txt for unparsable files.
+	go func() {
+		songHashesPath := path.Join(config.DataPath, "song_errors.txt")
+		f, err := os.OpenFile(songHashesPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
+		if err != nil {
+			panic(fmt.Errorf("%q: %v", songHashesPath, err))
+		}
+
+		i = 0
+		for songErr := range songsErrCh {
+			f.WriteString(fmt.Sprintf("%q\t%q\n",
+				songErr.Song.Path,
+				songErr.Err))
+		}
 	}()
 
 	// Consumes songs via SongsOutChan, collects hash and metadata into song_hashes.txt
@@ -91,16 +124,25 @@ func IdentifySongs(songs []*Song, outFile string) {
 		if err != nil {
 			panic(fmt.Errorf("%q: %v", songHashesPath, err))
 		}
+		csv := csv.NewWriter(f)
 
 		i = 0
 		for song := range songsOutCh {
 			meta := song.Meta()
-			f.WriteString(fmt.Sprintf("%s\t%q\t%q\t%q\t%q\n", song.Hash, song.Path, meta.Artist, meta.Album, meta.Title))
+
+			csv.Write([]string{
+				song.Hash,
+				song.Path,
+				meta.Artist,
+				meta.Album,
+				meta.Title})
+
 			i++
 			if i%100 == 0 {
 				log.Printf("Attempted identification of %d songs\n", i)
 			}
 		}
+		csv.Flush()
 		log.Printf("Identifying songs...done\n")
 	}()
 }
@@ -119,23 +161,23 @@ func audioFileChecksum(path string) (string, error) {
 }
 
 // if something goes wrong, you get a blank one.
-func metaData(path string) SongMeta {
+func metaData(path string) (SongMeta, error) {
 	var meta SongMeta
 
 	f, err := os.Open(path)
 	defer f.Close()
 	if err != nil {
-		return meta
+		return meta, err
 	}
 
 	m, err := tag.ReadFrom(f)
 	if err != nil {
-		return meta
+		return meta, err
 	}
 
 	meta.Title = m.Title()
 	meta.Album = m.Album()
 	meta.Artist = m.Artist()
 
-	return meta
+	return meta, nil
 }
